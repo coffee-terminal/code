8429cc9f09923733f3a9f3b5bf1076f116096183
Vibes Terminal <vibes.terminal@gmail.com>
2025-11-10 12:16:33 +0200

    js001

---
+++ 011 js/app.js
@@ -0,0 +1,25 @@
+console.log("Labas, Bebriau!");
+
+var x = 10;
+
+console.log(x);
+
+var pasisveikinimasSuBarsuku = "Labas rytas, ponas Barsukai!";
+
+console.log("pasisveikinimasSuBarsuku", pasisveikinimasSuBarsuku);
+x = 20;
+
+console.log(x, pasisveikinimasSuBarsuku);
+
+let L = 20;
+const N = 20;
+
+// 1. Kintamojo vardas
+// 2. Kintamojo reikšmė (turinys)
+// 3. Kintamojo tipas
+
+let K1 = 25; // tipas number integer
+let K2 = "Barsukas"; //tipas string symbol
+let K3 = "102"; //tipas string
+let K4 = 54.78; // tipas number
+let K8 = -78.0002; // tipas number
