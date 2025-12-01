0b5688089489bb0420aa030576f7a127ef480aa2
Vibes Terminal <vibes.terminal@gmail.com>
2025-12-01 17:25:38 +0200

    023-02

--- 023/app.js
+++ 023/app.js
@@ -1,51 +1,54 @@
-console.log('Labas, Apiau!');
+console.log('Labas Apiau!');
 
-fetch('https://jsonplaceholder.typicode.com/todos/1') // siuncia uzklausa
-    .then((response) => response.json()) // laukiam tada JSON gauta rezultata
+fetch('https://jsonplaceholder.typicode.com/todos/1') // siunčia užklausa
+    .then((response) => response.json()) // laukiam tada JSON gautą rezultatą
     .then((json) => console.log(json)); // kai yra JSON tada loginam
 
-//turiu objekta
+// Turiu objektą
 
 const namas = {
     kaminas: true,
     kambariai: 5,
-    adresas: 'Pievos g.',
+    adresas: 'Pievos g. 15',
 };
+
 console.log(namas);
 
-// Reikia perduoti per interneta, todel reikia versti i stringa(teksta)
+// Reikia perduoti per internetą, todėl reikia versti į stringą (tekstą)
 
 const objektasKaipStringas = JSON.stringify(namas);
 
 console.log(objektasKaipStringas);
 
-//Perduodam
+// Perduodam
 
-//Reikia vel atversti i objekta
+// Reikia vėl atversti į objektą
 
 const velNamas = JSON.parse(objektasKaipStringas);
 
 console.log(velNamas);
 
-const userUl = document.querySelector('#users-container');
+const usersUl = document.querySelector('#users-container');
+
 const printUserList = (users) => {
     users.forEach((user) => {
-        const li = document.createElement('li'); // tuscias li elementas
-        const userName = user.name; // paimu is strukturos name
-        const phrase = user.company.catchPhrase; // paimu fraze
+        const li = document.createElement('li'); // tuščias li elementas
+        const userName = user.name; // paimu iš struktūros name
+        const phrase = user.company.catchPhrase; // paimu frazę
         const id = user.id;
-        const idVat = (id * 21) / 100;
-        li.innerText = userName + ': ' + phrase; // i li dedu paimta name
+        const idVat = (id * 21) / 100; // čia matematika
+        li.innerText = userName + ': ' + phrase; // į li dedu paimtą name ir frazę
         li.classList.add('user');
-        userUl.append(li);
-        const liVat = document.createElement('li');
+        usersUl.appendChild(li);
+        const liVat = document.createElement('li'); // tuščias li elementas
         liVat.innerText = idVat;
-        li.classList.add('livat');
-        userUl.append(liVat);
+        liVat.classList.add('vated');
+        usersUl.appendChild(liVat);
     });
 };
-fetch('https://jsonplaceholder.typicode.com/users') // siuncia uzklausa
-    .then((res) => res.json()) // laukiam tada JSON gauta rezultata
+
+fetch('https://jsonplaceholder.typicode.com/users') // siunčia užklausa
+    .then((res) => res.json()) // laukiam tada JSON gautą rezultatą
     .then((users) => {
         console.log(users);
         printUserList(users);
