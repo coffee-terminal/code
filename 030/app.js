5300c84d8a4f9d4ea0a6e30b9bb4bf0ecd74c765
coffee-terminal <vibes.terminal@gmail.com>
2025-12-16 00:59:28 +0200

    12.15

--- 030/app.js
+++ 030/app.js
@@ -9,6 +9,15 @@ Store - veiksmas
 
 */
 
+/*   
+1. inicijuoja funkcija kuri pirma paziuri ar yra listas, jei yra rendina
+2. toje pacioje funcijoje yra migtukas kuris laukia kad nurodytu pasirinkta spalva
+3. pasirinkta spalva siunciama i Store funkcija kad papilnai butu aprasomas value objektas
+4. store funkcijja ideda objekta i sarasa ir kreipiasi i Write funkcija
+5. write funkcija pavercia sarasa stringu, ir prideda prie key jo value data
+6.  store kreipiasi i render funkcija, joje selectinama kur bus talpinama, po to  pasirenkama template pagal kuria klonuojama struktuka kiekvienam forEach ciklui
+
+ */
 let LIST;
 const KEY = 'myFancyColorsList';
 
@@ -52,6 +61,35 @@ const render = (_) => {
         const rowHtml = listRowTemplate.content.cloneNode(true);
         const colorSq = rowHtml.querySelector('[data-color-sq]');
 
+        //       const deleteButton = rowHtml.querySelector('[data-delete-sq]');
+
+        // colorItem.id sugeneruotas Store metode id
+        //dataset.id // i elementa prideda atributa 'data-id'
+        // dataset.id = colorItem.id atributui priskiria reiksme data-id = '1326623'
+        //  deleteButton.dataset.id = colorItem.id;
+        //deleteButton.addEventListener('click', (e) => {
+        // e eventas
+        // e.target is evento gautas paspaustas mygtuko elementas
+        // e.target.dataset kreipimasis i elemento 'data-' atributus
+        // e.target.dataset.id kreipimasis i atributa 'data-id;
+        //           const id = parseInt(e.target.dataset.id);
+        //           Destroy(id);
+        // });
+
+        ///* EDIT */
+
+        // const editInput = rowHtml.querySelector('[data-edit-color-input]');
+        // const editButton = rowHtml.querySelector('[data-edit-color-button]');
+        // editInput.value = colorItem.color; // senu duomenu perrrasymas i edit forma
+        // editButton.dataset.id = colorItem.id;
+
+        // editButton.addEventListener('click', (e) => {
+        //     const id = parseInt(e.target.dataset.id);
+        //     const color = editInput.value;
+
+        //     Update(id, color);
+        // });
+
         colorSq.style.backgroundColor = colorItem.color + '70'; // + permatomumas
         colorSq.style.borderColor = colorItem.color;
 
@@ -71,6 +109,34 @@ const Store = (data) => {
         color: data,
     };
     LIST.push(dataToStore);
+    writeLocalStorage();
+    render();
+};
+
+/* 
+
+//Destroy pas mus vykdo 'daikto' pasalinima is saugyklos 
+//Turi gauti 'daikto' indentifikatoriu  
+// Turi pasalinti daikta su nurodytu identifikatoriumi 
+
+*/
+
+const Destroy = (id) => {
+    LIST = LIST.filter((color) => color.id != id); //ismetam is saraso kvadratuka su nurodytu id
+    writeLocalStorage();
+    render();
+};
+
+/* 
+Update vykdo redaguoto 'daikto' saugojima saugykloje 
+Turi gauti 'daikto' indentifikatoriu ir daikto naujas savybes
+Turi persaugoti daikta su nurodytu indentifikatoriumi ir naujom savybem
+
+
+
+ */
+const Update = (id, data) => {
+    LIST = LIST.map((item) => (item.id == id ? { ...item, color: data } : item));
     writeLocalStorage();
     render();
 };
---
+++ nd/CRUD/app.js
@@ -0,0 +1,156 @@
+const rand = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
+/* 
+
+Ganykla. Turim trijų rūšių gyvulius: avis, antis ir antilopes. 
+Kiekvienas gyvulys turi savo svorį.
+Parašyti localStorage CRUD aplikaciją, 
+kurioje būtų galima pridėti naujus gyvulius su jų svoriais į ganyklą,
+ ištrinti iš ganyklos 
+ ir redaguoti kiekvieno jų svorį.
+ */
+
+/*   
+1. inicijuoja funkcija kuri pirma paziuri ar yra listas, jei yra rendina
+2. toje pacioje funcijoje yra migtukas kuris laukia kad nurodytu pasirinkta spalva
+3. pasirinkta spalva siunciama i Store funkcija kad papilnai butu aprasomas value objektas
+4. store funkcijja ideda objekta i sarasa ir kreipiasi i Write funkcija
+5. write funkcija pavercia sarasa stringu, ir prideda prie key jo value data
+6.  store kreipiasi i render funkcija, joje selectinama kur bus talpinama, po to  pasirenkama template pagal kuria klonuojama struktuka kiekvienam forEach ciklui
+
+
+*/
+
+let LIST;
+const KEY = 'gyvuliai';
+let pasirinktas;
+const animalInput = document.querySelector('[data-animal-input]');
+const weightInput = document.querySelector('[data-weight-button]');
+
+animalInput.addEventListener('change', (_) => {
+    return (pasirinktas = animalInput.value);
+});
+
+const init = (_) => {
+    readLocalStorage();
+    render();
+    // const animalInput = document.querySelector('[data-animal-input]');
+    const animalAddButton = document.querySelector('[data-add-animal-button]');
+    // const weightInput = document.querySelector('[data-weight-button]');
+
+    animalAddButton.addEventListener('click', (_) => {
+        const animal = animalInput.value;
+        const svorisAnimal = weightInput.value;
+        Store(animal, svorisAnimal);
+    });
+};
+
+const readLocalStorage = (_) => {
+    let data = localStorage.getItem(KEY);
+    if (null === data) {
+        LIST = [];
+    } else {
+        LIST = JSON.parse(data);
+    }
+};
+
+const writeLocalStorage = (_) => {
+    let data = JSON.stringify(LIST);
+    localStorage.setItem(KEY, data);
+};
+
+const render = (_) => {
+    const listBin = document.querySelector('[data-colors-list]');
+    const listRowTemplate = document.querySelector('[data-list-template]');
+    listBin.innerHTML = '';
+    LIST.forEach((animal) => {
+        const rowHtml = listRowTemplate.content.cloneNode(true);
+        const animalId = rowHtml.querySelector('[data-animal-id]');
+        animalId.innerHTML = `${animal.gyvunas} ${animal.svoris} `;
+        animalId.style.width = '40%';
+        animalId.style.backgroundColor = 'rgb(255,255,255,0.2)';
+        animalId.style.lineHeight = '40px';
+        animalId.style.borderBottom = '1px solid white';
+        listBin.appendChild(rowHtml);
+    });
+};
+
+const Store = (data, meta) => {
+    if ('' != pasirinktas && weightInput.value > 0) {
+        const id = rand(10000000, 99999999);
+        dataToStore = {
+            id,
+            gyvunas: data,
+            svoris: meta,
+        };
+
+        LIST.push(dataToStore);
+        writeLocalStorage();
+        render();
+    }
+};
+
+init();
+
+/* ==============================================================================================
+==============================================================================================
+==============================================================================================
+==============================================================================================
+============================================================================================== */
+
+// const renderAnimalList = (_) => {
+//     const ol = document.querySelector('#ol');
+//     ol.innerHTML = '';
+//     animalKey.forEach((animal) => {
+//         const li = document.createElement('li');
+//         li.style.width = '40%';
+//         li.style.backgroundColor = 'rgb(255,255,255,0.2)';
+//         li.innerHTML = animal;
+//         li.style.lineHeight = '40px';
+//         li.style.borderBottom = '1px solid white';
+//         ol.append(li);
+//     });
+// };
+
+// let animalKey;
+
+// animalKey = localStorage.getItem('gyvuliai');
+
+// if (null === animalKey) {
+//     animalKey = [];
+// } else {
+//     animalKey = JSON.parse(animalKey);
+//     console.log(animalKey);
+// }
+
+// const animalsList = document.querySelector('select');
+// animalsList.style.backgroundColor = 'lightgray';
+// animalsList.style.color = 'black';
+// animalsList.style.border = '1px solid white';
+
+// const animalsSvoris = document.querySelector('#svoris');
+// animalsSvoris.style.backgroundColor = 'lightgray';
+// animalsSvoris.style.border = '1px solid white ';
+// animalsSvoris.style.width = '60px';
+// animalsSvoris.style.color = 'black';
+
+// const pridetiButton = document.querySelector('#pridetiButton');
+
+// animalsList.addEventListener('change', (_) => {
+//     pasirinktas = animalsList.value;
+// });
+
+// pridetiButton.addEventListener('click', (_) => {
+//     if ('' != pasirinktas && animalsSvoris.value > 0) {
+//         animalKey.push(`${pasirinktas} ${animalsSvoris.value} kg`);
+//         const animalValue = JSON.stringify(animalKey);
+//         localStorage.setItem('gyvuliai', animalValue);
+//     }
+
+//     renderAnimalList();
+// });
+
+// renderAnimalList();
