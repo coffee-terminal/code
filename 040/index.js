0d5a650481d16752c6b78a7f88c823f100ea178e
coffee-terminal <vibes.terminal@gmail.com>
2026-01-05 13:33:34 +0200

    express js 040

---
+++ 040/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const fs = require('fs');
+const { title } = require('process');
+const app = express();
+
+const port = 80;
+//Failai folderyje 'public' bus pasiekiami per narsykle
+app.use(express.static('public')); // Nurodome, kad statiniai failai bus is 'public' katalogo
+
+// Dalis Router, '/' home
+app.get('/', (req, res) => {
+    //skaitome tris atskirus failus. Toks skaitymas galimas tiktai backend
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const home = fs.readFileSync('./html/home.html', 'utf8');
+
+    const pageTitle = 'Home Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + home + bottom); //vienas html failas is sujungtu failu
+});
+
+app.get('/services', (req, res) => {
+    //skaitome tris atskirus failus. Toks skaitymas galimas tiktai backend
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const services = fs.readFileSync('./html/services.html', 'utf8');
+
+    const pageTitle = 'Services Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + services + bottom); //vienas html failas is sujungtu failu
+});
+
+app.get('/about', (req, res) => {
+    //skaitome tris atskirus failus. Toks skaitymas galimas tiktai backend
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const about = fs.readFileSync('./html/about.html', 'utf8');
+
+    const pageTitle = 'About Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + about + bottom); //vienas html failas is sujungtu failu
+});
+
+app.get('/contact', (req, res) => {
+    //skaitome tris atskirus failus. Toks skaitymas galimas tiktai backend
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const contact = fs.readFileSync('./html/contact.html', 'utf8');
+    const pageTitle = 'Contact Page';
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    res.send(topWithTitle + contact + bottom); //vienas html failas is sujungtu failu
+});
+
+// Paleidžia serverį ir parašo terminale, kad viskas yra gerai.
+app.listen(port, () => {
+    console.log(`Viskas gerai. Bebras dirba ant ${port} porto`);
+});
