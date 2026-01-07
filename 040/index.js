dd751c1d1d6ab7d66bb792018afe707aebadf118
coffee-terminal <vibes.terminal@gmail.com>
2026-01-07 12:38:06 +0200

    042 parametrai

--- 040/index.js
+++ 040/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const { title } = require('process');
 const app = express();
+//importuojame services.js duomenis
+const { services } = require('./services');
 
 const port = 80;
 //Failai folderyje 'public' bus pasiekiami per narsykle
@@ -18,6 +20,44 @@ app.get('/', (req, res) => {
     const topWithTitle = top.replace('{{title}}', pageTitle);
 
     res.send(topWithTitle + home + bottom); //vienas html failas is sujungtu failu
+});
+
+app.get('/service/:slug', (req, res) => {
+    // Iš URL paimame slug parametrą
+    const slug = req.params.slug;
+    // Randame atitinkamą paslaugą pagal slug
+    const service = services.find((s) => s.slug === slug);
+
+    // Tikriname, ar paslauga rasta
+    if (!service) {
+        // Jei paslauga nerasta, grąžiname 404 klaidą
+        return res.status(404).send('Service not found');
+    }
+
+    // skaitome tris atskirus failus. Toks skaitymas galimas tik backend'e
+    const top = fs.readFileSync('./html/top.html', 'utf8');
+    const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+    const serviceHtml = fs.readFileSync('./html/service.html', 'utf8');
+
+    // Sukuriame HTML sąrašą iš paslaugos savybių
+    let features = '';
+    service.features.forEach((feature) => {
+        features += `<li>${feature}</li>`;
+    });
+
+    // Pakeičiame vietas HTML faile su faktiniais duomenimis
+    let servicePage = serviceHtml
+        .replace('{{icon}}', service.icon)
+        .replace('{{title}}', service.title)
+        .replace('{{description}}', service.description)
+        .replace('{{features}}', features);
+
+    // Pakeičiame puslapio pavadinimą
+    const pageTitle = service.title;
+    const topWithTitle = top.replace('{{title}}', pageTitle);
+
+    // Siunčiame galutinį HTML atsakymą
+    res.send(topWithTitle + servicePage + bottom);
 });
 
 app.get('/services', (req, res) => {
